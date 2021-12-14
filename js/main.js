4329fbd495f20a4eb592f3dc69925646875ce296
Bohdan Maslow <bohdanmaslowsfcc@gmail.com>
2021-12-14 18:16:36 +0200

    last changes

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ $(function(){
 $.ajax({
   url: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json",
 }).done(function(data) {
-    for (var i = 0; i < data.length; i++){
-        $(".js-loop").append('<div class="col-4">' + data[i].rate + '</div>'
-            + '<div class="col-4">' + data[i].txt + '</div >'
-            + '<div class="col-4">' + data[i].cc + '</div >');
-    }
-
+  for (var i = 0; i < data.length; i++) {
+    $('.sel').prepend('<option data-rate='+ data[i].rate+'>' + data[i].txt + '</option>');
+      
+  }
 });
+  $('.sel').on('change', function () {
+    var curRate = $(this).children("option:selected").attr('data-rate');
+    $(".curRate").text(curRate)
+    console.log(curRate);
 
+    
+   })
 });
