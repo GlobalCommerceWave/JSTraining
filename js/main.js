0196085bfd303dd01bc48ff97fbc612bfe1117d7
Yuliana_Kravchuk <yuliana_kravchuk@epam.com>
2021-12-14 18:20:08 +0200

    revert to basic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 $(function () {
     // your code
-    $.ajax({
-        url: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json",
-    }).done(function (data) {
-        console.log(data);
-
-        for (var i = 0; i < data.length; i++) {
-            $(".js-loop").append(
-                '<div class="col-4">' +
-                    data[i].rate +
-                    "</div>" +
-                    '<div class="col-4">' +
-                    data[i].txt +
-                    "</div>" +
-                    '<div class="col-4">' +
-                    data[i].cc +
-                    "</div>"
-            );
-        }
-    });
+    
 });
