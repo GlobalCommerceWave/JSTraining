56c6a8e40b64d4381192d7f8ec13e49f1c810346
Bohdan Maslow <bohdanmaslowsfcc@gmail.com>
2021-12-16 18:12:15 +0200

    add-drop-drag-patch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(function () {
-    // your code
-    
+    $(".block1").draggable();
+
+    $(".block2").droppable({
+        drop: function (event, ui) {
+            $(".topLine").css("background-color", "white");
+            $(".mainBlock").css("background-color", "white");
+        },
+        out: function (event, ui) {
+            $(".topLine").css("background-color", "black");
+            $(".mainBlock").css("background-color", "black");
+        },
+    });
 });
