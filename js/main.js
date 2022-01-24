48c4e00ad02dc720ad1971549b034ec57cd060c8
Yaroslav Maslov <yaroslavmaslov1221@gmail.com>
2022-01-24 18:10:30 +0200

    yl-002:fix rates page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 $(function () {
     // your code
+    /*
     $.ajax({
         url: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json",
     }).done(function (data) {
         console.log(data);
 
         for (var i = 0; i < data.length; i++) {
-            $(".js-loop").append(
-                '<div class="col-4">' +
+            $("select").append(
+                "<option data-rate=" +
                     data[i].rate +
-                    "</div>" +
-                    '<div class="col-4">' +
+                    ">" +
                     data[i].txt +
-                    "</div>" +
-                    '<div class="col-4">' +
+                    " " +
                     data[i].cc +
-                    "</div>"
+                    "</option>"
             );
         }
     });
+    $("select").on("change", function () {
+        var curRate = $(this).children("option:selected").attr("data-rate");
+        $("h1").text(curRate);
+    });
 });
+*/
